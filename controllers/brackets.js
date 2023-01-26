d721a0b6af5beaa9708de59e7137138b1372dfc0
RemixFX <remix.master@yandex.ru>
2023-01-26 14:42:33 +0300

    feat: get method for brackets requests

--- controllers/brackets.js
+++ controllers/brackets.js
@@ -3,9 +3,7 @@ const db = require('../db');
 const postBracket = async (req, res, next) => {
   try {
     for (const player of req.body) {
-
       const { id, role, name, class_name, race, ilvl, note, group_name, raid_id } = player;
-      console.log(player)
       await db.query(
         `INSERT INTO brackets (id, role, name, class_name, race, ilvl, note, group_name, raid_id)
      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`,
@@ -17,6 +15,13 @@ const postBracket = async (req, res, next) => {
     next(err)
   }
 }
+
+const getBrackets = (req, res, next) => {
+  db.query(
+    'SELECT * FROM brackets')
+    .then((brackets) => res.status(200).json(brackets.rows))
+    .catch((err) => next(err))
+};
 
 
 module.exports = { postBracket, getBrackets }
